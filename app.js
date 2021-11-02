e523cf77a2b80a5f9bff9a37f42bb0f086e34e3b
S545020 <S545020@NWMISSOURI.EDU>
2021-11-02 16:42:11 -0500

    added selector

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var umbrellaRouter = require('./routes/umbrella');
 var addmodsRouter = require('./routes/addmods');
+var selectorRouter = require('./routes/selector');
 var app = express();
 
 // view engine setup
@@ -24,7 +25,7 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/umbrella', umbrellaRouter);
 app.use('/addmods', addmodsRouter);
-
+app.use('/selector', selectorRouter);
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
