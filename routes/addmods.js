221102d63f9a452afbf21a940453b7cf1545e537
S545020 <S545020@NWMISSOURI.EDU>
2021-11-02 19:23:48 -0500

    Bonus

--- routes/addmods.js
+++ routes/addmods.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-    let query = req.query
+    var query = req.query
 console.log(`rows ${query.rows}`)
 console.log(`cols ${query.cols}`)
 
