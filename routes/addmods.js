f41443c2744c43f83559f30a3e506ea5f9ca4cb0
S545020 <S545020@NWMISSOURI.EDU>
2021-11-02 20:18:22 -0500

    change in part7

--- routes/addmods.js
+++ routes/addmods.js
@@ -6,7 +6,8 @@ router.get('/', function(req, res, next) {
     var query = req.query
 console.log(`rows ${query.rows}`)
 console.log(`cols ${query.cols}`)
-
+var rows=Number(query.rows)
+var cols=Number(query.cols)
     res.render('addmods', { title: 'addmods',query: query });
 });
 
